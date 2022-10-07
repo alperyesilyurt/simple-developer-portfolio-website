b26bcda44f8b0a3263e4360874fc545bb6f1c01c
Alper Yesilyurt <alperesilyurt@hotmail.com>
2022-10-07 15:38:26 +0300

    change some feat

--- components/Hero.js
+++ components/Hero.js
@@ -13,12 +13,12 @@ export default function Hero() {
         <RoughNotationGroup show={true}>
           <RainbowHighlight color={colors[0]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Developer.
+              Engineer.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[1]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Designer.
+              Developer.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[2]}>
@@ -28,7 +28,7 @@ export default function Hero() {
           </RainbowHighlight>
           <RainbowHighlight color={colors[3]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Youtuber.
+              Learner.
             </h1>
           </RainbowHighlight>
         </RoughNotationGroup>
