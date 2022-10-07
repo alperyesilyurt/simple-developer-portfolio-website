2b9018bb38d42ef5f4c1d7300f205be7123a6eed
Alper Yesilyurt <alperesilyurt@hotmail.com>
2022-10-07 16:07:23 +0300

    update

--- constants/data.js
+++ constants/data.js
@@ -34,35 +34,50 @@ const userData = {
       desc: "Developed, installed, and configured internal web applications, as well as new application releases into test and production environments.",
     },
     {
-      title: "Software Engineer Intern",
-      company: "Hepsiburada",
-      year: "2022",
-      companyLink: "https://www.hepsiburada.com/",
-      desc: "Frontend web programming training, mastery of agile working concept.",
-    },
-    {
-      title: "Undergraduate Researcher",
-      company: "TÜBİTAK",
-      year: "2021-2022",
-      companyLink: "https://www.tubitak.gov.tr/",
-      desc: "Completed six-month undergraduate researcher. Developed a ML solution for Handover process for next generation networks.",
-    },
-    {
       title: "Graduation",
       company: "ISTANBUL TECHNICAL UNIVERSITY",
       year: "2022",
       companyLink: "https://www.itu.edu.tr/",
       desc: "Major in Electronics and Communication Engineering with a GPA of 3.22 Nobody asks this but it's okay.",
     },
+    {
+      title: "Software Engineer Intern",
+      company: "Hepsiburada",
+      year: "2022",
+      companyLink: "https://www.hepsiburada.com/",
+      desc: "Learn agile approach techniques. I used HTML, CSS, Javascript, Vuejs, GitHub and Jira while working with my team. I work as and frontend web developer. Also with my mentor, I learned the basics of computer science",
+    },
+    {
+      title: "Undergraduate Researcher",
+      company: "TÜBİTAK",
+      year: "2022",
+      companyLink: "https://www.tubitak.gov.tr/",
+      desc: "Completed six-month undergraduate researcher. Developed a ML solution for Handover process for next generation networks.",
+    },
+    {
+      title: "Intern",
+      company: "ITU THAL LAB",
+      year: "2021",
+      companyLink: "https://www.thal.itu.edu.tr/",
+      desc: "I established a communication system on the Gnu Radio application and provided data transmission with different modulation techniques. Then I created a synchronization block through codeable blocks on Gnu Radio. This sync block was for Preamble Sync, and while doing it, I coded it in Python and did a literature search.",
+    },
+    {
+      title: "Intern",
+      company: "TÜBİTAK",
+      year: "2021",
+      companyLink: "https://www.tubitak.gov.tr/",
+      desc: "I made Network Time Protocol Synchronization using three Raspberry Pi. In doing this, I used three different Adalm Pluto SDRs and these SDRs each have two transmitters and receivers. The main purpose of the project was to provide synchronization with four different timestamps by communicating with SDRs.",
+    },
+
   ],
   resumeUrl:
     "https://drive.google.com/file/d/1KawRlFS0UH-J8GreZQh_bXcUHoPXHUmK/view?usp=sharing",
   socialLinks: {
-    instagram: "https://instagram.com/",
-    twitter: "https://twitter.com/",
+    instagram: "https://instagram.com/alperesilyurt",
+    twitter: "https://twitter.com/BeeEngineer",
     linkedin: "https://www.linkedin.com/in/alper-yesilyurt-489091149/",
     github: "https://github.com/alperyesilyurt",
-    facebook: "https://facebook.com/",
+    facebook: "https://facebook.com/alper.yesilyurt.52",
   },
 };
 
